6366f043ea09522968d6bd322451a08cb1b3f2cd
Mathias Elgaard <matelgaard@proton.me>
2025-12-08 09:46:20 +0100

    Add timeline visualization

---
+++ SEP1_Music/Assets/Scripts/Metronome.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Metronome : MonoBehaviour
+{
+    public Timeline timeline;
+    public AudioClip beatSoundClip;
+    public AudioClip semiBeatSoundClip;
+    public double scheduleAhead = 0.2;
+
+    private double nextEventTime;
+    private int source = 0;
+    private AudioSource[] audioSources = new AudioSource[8];
+
+    void Start()
+    {
+        for (int i = 0; i < audioSources.Length; i++)
+        {
+            GameObject child = new GameObject("AudioPlayer");
+            child.transform.parent = transform;
+            audioSources[i] = child.AddComponent<AudioSource>();
+        }
+
+        nextEventTime = AudioSettings.dspTime;
+    }
+
+    void Update()
+    {
+        double time = AudioSettings.dspTime;
+
+        if (time + scheduleAhead > nextEventTime)
+        {
+            int beat = (int)(timeline.ToBeats(nextEventTime));
+            int subdivision = beat % timeline.beatsPerMeasure;
+
+            audioSources[source].clip = subdivision == 0 ? beatSoundClip : semiBeatSoundClip;
+            audioSources[source].PlayScheduled(nextEventTime);
+            source = (source + 1) % audioSources.Length;
+
+            Debug.Log("Scheduled source " + source + " to start at time " + nextEventTime + " (beat " + beat + ")");
+
+            nextEventTime = timeline.ToSeconds(beat + 1.0);
+        }
+    }
+}
--- SEP1_Music/Assets/Scripts/TimelineDisplay.cs
+++ SEP1_Music/Assets/Scripts/TimelineDisplay.cs
@@ -4,22 +4,100 @@ using UnityEngine;
 public class TimelineDisplay : MonoBehaviour
 {
     public int maxBars = 4;
-    public int beatsPerMeasure = 4;
+    public double distancePerMeasure = 4.0f;
 
     public GameObjectPool barLines;
     public GameObjectPool semiBarLines;
 
+    private Timeline timeline;
+    private List<Transform> barLineTransforms;
+    private List<Transform> semiBarLineTransforms;
+
+    private double ToBeats(float displacement)
+    {
+        return 4.0 * (double)displacement / distancePerMeasure;
+    }
+
+    private float ToDisplacement(double beats)
+    {
+        return (float)(0.25 * distancePerMeasure * beats);
+    }
+
     void Awake()
     {
+        timeline = GetComponent<Timeline>();
+        barLineTransforms = new List<Transform>();
+        semiBarLineTransforms = new List<Transform>();
     }
 
     void Start()
     {
         barLines.Allocate(maxBars);
-        semiBarLines.Allocate((beatsPerMeasure - 1) * maxBars);
+        semiBarLines.Allocate((timeline.beatsPerMeasure - 1) * maxBars);
     }
 
     void Update()
     {
+        float yMin = -5.0f;
+        float yMax = 5.0f;
+
+        int startingBeat = (int)(timeline.beat + ToBeats(yMin));
+        int endingBeat = (int)(timeline.beat + ToBeats(yMax));
+        int beatsAmount = endingBeat - startingBeat + 1;
+
+        int bi = 0;
+        int sbi = 0;
+        for (int i = 0; i < beatsAmount; i++)
+        {
+            int beat = startingBeat + i;
+            Transform b;
+
+            if (beat % timeline.beatsPerMeasure == 0)
+            {
+                if (bi == barLineTransforms.Count)
+                {
+                    GameObject barLine;
+                    barLine = barLines.Acquire();
+                    barLineTransforms.Add(barLine.transform);
+                    b = barLine.transform;
+                }
+                else
+                {
+                    b = barLineTransforms[bi];
+                }
+
+                bi++;
+            }
+            else
+            {
+                if (sbi == semiBarLineTransforms.Count)
+                {
+                    GameObject barLine;
+                    barLine = semiBarLines.Acquire();
+                    semiBarLineTransforms.Add(barLine.transform);
+                    b = barLine.transform;
+                }
+                else
+                {
+                    b = semiBarLineTransforms[sbi];
+                }
+
+                sbi++;
+            }
+
+            b.position = new Vector3(b.position.x, ToDisplacement(beat - timeline.beat), b.position.z);
+        }
+
+        for (int i = bi; i < barLineTransforms.Count; i++)
+        {
+            barLines.Release(barLineTransforms[i].gameObject);
+            barLineTransforms.Remove(barLineTransforms[i]);
+        }
+
+        for (int i = sbi; i < semiBarLineTransforms.Count; i++)
+        {
+            semiBarLines.Release(semiBarLineTransforms[i].gameObject);
+            semiBarLineTransforms.Remove(semiBarLineTransforms[i]);
+        }
     }
 }
