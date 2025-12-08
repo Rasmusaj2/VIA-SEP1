33b11698d6bcbbc1b5ffa249c4f169bf7e2bdfe1
Mathias Elgaard <matelgaard@proton.me>
2025-12-08 16:22:45 +0100

    Audio latency compensation in metronome

--- SEP1_Music/Assets/Scripts/Metronome.cs
+++ SEP1_Music/Assets/Scripts/Metronome.cs
@@ -5,6 +5,8 @@ public class Metronome : MonoBehaviour
     public Timeline timeline;
     public AudioClip beatSoundClip;
     public AudioClip semiBeatSoundClip;
+    [Range(0.0f, 1000.0f)]
+    public double audioDelayCompensationMilliseconds = 40.0;
     [Min(0)]
     public double scheduleAhead = 0.2;
 
@@ -26,13 +28,15 @@ public class Metronome : MonoBehaviour
 
     void Update()
     {
+        double audioDelayCompensationSeconds = 0.001 * audioDelayCompensationMilliseconds;
+
         if (timeline.time + scheduleAhead > nextEventTime)
         {
             int beat = (int)(timeline.ToBeats(nextEventTime));
             int subdivision = beat % timeline.beatsPerMeasure;
 
             audioSources[source].clip = subdivision == 0 ? beatSoundClip : semiBeatSoundClip;
-            audioSources[source].PlayScheduled(timeline.ToRealTime(nextEventTime));
+            audioSources[source].PlayScheduled(timeline.ToRealTime(nextEventTime) - audioDelayCompensationSeconds);
             source = (source + 1) % audioSources.Length;
 
             Debug.Log("Scheduled source " + source + " to start at time " + nextEventTime + " (beat " + beat + ")");
